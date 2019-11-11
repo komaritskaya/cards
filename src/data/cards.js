af90416e8830c8c37f35d3efa3356095f7e407d5
Nadezhda Komaritckaia <komaritskaya@Nadezhdas-MacBook-Pro.local>
2019-11-11 04:07:46 +0300

    task completed

---
+++ src/data/cards.js
@@ -0,0 +1,16 @@
+export default [
+  {
+    img: 'http://lorempixel.com/100/100/',
+    title: 'Kristy',
+    text: 'Kristy is an art director living in New York.',
+  },
+  {
+    img: 'http://lorempixel.com/150/150/',
+    title: 'Molly',
+    text: 'Molly is a personal assistant living in Paris.',
+  },
+  {
+    title: 'Matt Giampietro',
+    text: 'Matthew is an interior designer living in New York.',
+  },
+];
